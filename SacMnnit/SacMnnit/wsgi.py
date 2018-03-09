91bc272b55bc5d137f208a4c9e2ccc3f3d7d6f7d
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-10 02:20:32 +0530

    modified_wsgi.py

--- SacMnnit/SacMnnit/wsgi.py
+++ SacMnnit/SacMnnit/wsgi.py
@@ -11,6 +11,21 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+#added for heroku
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SacMnnit.settings")
 
 application = get_wsgi_application()
+
+#added for heroku
+application = DjangoWhiteNoise(application)
+
+
+
+
+
+
+
+
+
