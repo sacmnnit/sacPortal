be523cf3871e241675eab0669e954b288fc7d273
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-09 22:21:55 +0530

    done_technical_events

--- SacMnnit/Technical/views.py
+++ SacMnnit/Technical/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def avishkar(request):
+	return render(request,'Technical/avishkar.html', {})
+	
+def technological(request):
+	return render(request,'Technical/technological.html', {})
