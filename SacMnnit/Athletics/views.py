737f2a342c2221cf58e7763c55cef45c67dfe61f
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-09 22:24:48 +0530

    adding_athletics_events

--- SacMnnit/Athletics/views.py
+++ SacMnnit/Athletics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def annual_athletics(request):
+	return render(request,'Athletics/athletic-meet.html', {})
+
+def josh(request):
+	return render(request,'Athletics/josh.html', {})
+
+def games(request): 
+	return render(request,'Athletics/games.html', {})
+
+def yoga(request): 
+	return render(request,'Athletics/yoga.html', {})
