916366b1830ad5a959a44dfefcdb31fbbe1b3f08
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-09 22:23:31 +0530

    done_cultural

--- SacMnnit/Cultural/views.py
+++ SacMnnit/Cultural/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def culrav(request):
+	return render(request,'Culural/culrav.html', {})
+	
+def eloquence(request):
+	return render(request,'Cultural/eloquence.html', {})
+	
+def literary(request):
+	return render(request,'Cultural/literary.html', {})
+	
+def cultural(request):
+	return render(request,'Cultural/cultural.html', {})
+	
+def classical(request): 
+	return render(request,'Cultural/classical.html', {})
