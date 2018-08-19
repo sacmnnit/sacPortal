e23a3a20a2ea2c7fd574acd5fe76da1a4e64a85c
Deepak Bharti <deepakbharti823@gmail.com>
2018-08-19 18:52:46 +0530

    Update settings.py

--- SacMnnit/settings.py
+++ SacMnnit/settings.py
@@ -44,7 +44,7 @@ ADMINS = (
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'wocmnnit@gmail.com'#os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = 'weekendofcode@mnnit'#os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 
 ''' 
